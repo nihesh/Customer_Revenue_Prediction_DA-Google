05a0ae8301f611264f005a73e3474e18d75af5ed
nihesh <nihesh16059@iiitd.ac.in>
2018-12-02 17:59:51 +0530

    Neural Network best

--- train_regression.py
+++ train_regression.py
@@ -1,22 +1,24 @@
 import pickle 
 from sklearn.linear_model import LinearRegression
 from sklearn.neural_network import MLPRegressor
+from sklearn.model_selection import train_test_split
 import math
 import numpy as np
 from keras.layers import Dense,Conv2D, Flatten
 from keras.models import Sequential
 import csv
+from sklearn.metrics import mean_squared_error as MSE
 
 if(__name__ == "__main__"):
 
     file = open("./Processed_Data/train_processed.df-dump","rb")
     data = pickle.load(file)
-    
-    file = open("./Processed_Data/test_processed.df-dump","rb")
-    data_test = pickle.load(file)
+     # Target Label
+    Y = np.asarray(data['transactionRevenue'])
+    dftrain, dftest, Ytrain, Ytest = train_test_split(data, Y, test_size=0.3, random_state = 45)
     
     # training data dictionary with unique user ids
-    fullVisitorId = np.asarray(data_test['fullVisitorId'])
+    fullVisitorId = np.asarray(dftest['fullVisitorId'])
     user_dict = {}
     for i in range(0, len(fullVisitorId)):
         if fullVisitorId[i] in user_dict.keys():
@@ -29,67 +31,82 @@ if(__name__ == "__main__"):
     Y = np.asarray(data['transactionRevenue'])
 
     DROP = ['transactionRevenue', 'fullVisitorId', 'sessionId', 'visitId', 'visitStartTime']
-    data = data.drop(DROP, axis = 1)
-    DROP_TEST = ['fullVisitorId', 'sessionId', 'visitId', 'visitStartTime']
-    data_test = data_test.drop(DROP_TEST, axis = 1)
+    dftrain = dftrain.drop(DROP, axis = 1)
+    dftest = dftest.drop(DROP, axis = 1)
     
 	 # Input features
-    X = data.values
-    for i in range(len(X)):
-        for j in range(len(X[0])):
-            if(math.isnan(float(X[i][j]))):
-                X[i][j] = 0
+    Xtrain = dftrain.values
+    for i in range(len(Xtrain)):
+        for j in range(len(Xtrain[0])):
+            if(math.isnan(float(Xtrain[i][j]))):
+                Xtrain[i][j] = 0
             else:
-                X[i][j] = int(X[i][j])
+                Xtrain[i][j] = int(Xtrain[i][j])
                 
-    X_t = data_test.values
-    for i in range(len(X_t)):
-        for j in range(len(X_t[0])):
-            if(math.isnan(float(X_t[i][j]))):
-                X_t[i][j] = 0
+    # Input features
+    Xtest = dftest.values
+    for i in range(len(Xtest)):
+        for j in range(len(Xtest[0])):
+            if(math.isnan(float(Xtest[i][j]))):
+                Xtest[i][j] = 0
             else:
-                X_t[i][j] = int(X_t[i][j])
+                Xtest[i][j] = int(Xtest[i][j])
 
 	 # Model Selection
     # model = MLPRegressor(hidden_layer_sizes = (10,10,10)) 1.7
     model = Sequential()
-    model.add(Dense(30, activation = "relu"))
-    model.add(Dense(30, activation = "relu"))
-    model.add(Dense(30, activation = "relu"))
-    model.add(Dense(30, activation = "relu"))
+    model.add(Dense(10, activation = "relu"))
+    model.add(Dense(10, activation = "relu"))
     model.add(Dense(1, activation = "relu"))
     model.compile(optimizer = "rmsprop", loss="mean_squared_error", metrics=["accuracy"])
 
     # train error
-    model.fit(X, Y)
-    rev_pred = model.predict(X_t)
+    model.fit(Xtrain, Ytrain)
 
-    keys = list(user_dict.keys())
-    sums = []
-    
-    submit = csv.reader(open("sample_submission.csv"))
-    file = list(submit)
-    
-    for i in range (1, len(file)):
-        list_ind = user_dict[file[i][0]]
+    print("Training complete")
+
+    rev_pred = model.predict(Xtest)
+
+    y_trans_rev = np.asarray(Ytest)
+    ids =  list(user_dict.keys())
+    y_test = {}
+    #Actual values: Log of revenue sums
+    for i in range (0, len(ids)):
+        list_val = user_dict[ids[i]]
         sum = 0
-        if (len(list_ind) > 1):
-            for j in range (0, len(list_ind)):
-                sum = sum + math.exp(rev_pred[j])
-            sums.append(sum)
-            if (sum+1)<=0:
+        if (len(list_val) > 1):
+            for j in range (0, len(list_val)):
+                sum = sum + math.exp(y_trans_rev[list_val[j]])
+            if sum <= 0:
                 sum = 0
-            file[i][1] = float(math.log(sum + 1))
+            y_test[ids[i]] = float(math.log(sum + 1))
         else:
-            sums.append(rev_pred[list_ind[0]])
-            file[i][1] = rev_pred[list_ind[0]]
-            try:
-                file[i][1] = rev_pred[list_ind[0]][0] 
-            except:
-                pass
+            y_test[ids[i]] = y_trans_rev[list_val[0]]
+    
+    """ Dict of predicted values"""        
+    y_trans_rev_pred = np.asarray(rev_pred)
+    ids =  list(user_dict.keys())
+    y_pred = {}
+    #Predicted values: Log of revenue sums
+    for i in range (0, len(ids)):
+        list_val = user_dict[ids[i]]
+        sum = 0
+        if (len(list_val) > 1):
+            for j in range (0, len(list_val)):
+                sum = sum + math.exp(y_trans_rev_pred[list_val[j]])
+            if sum <= 0:
+                sum = 0
+            y_pred[ids[i]] = float(math.log(sum + 1))
+        else:
+            y_pred[ids[i]] = y_trans_rev_pred[list_val[0]]
             
-    with open('final.csv', 'w', newline='') as writeFile:
-        writer = csv.writer(writeFile)
-        writer.writerows(file)
-            
-    writeFile.close()
+    """ Calculating RMSE """
+    pred = []
+    real = []
+    for i in range (0, len(ids)):
+        pred.append(y_pred[ids[i]])
+        real.append(y_test[ids[i]])
+        
+    rmse = np.sqrt(MSE(real, pred))
+        
+    print("RMSE is",rmse) 
